ffdae5516d15358b794205610250b33d41569377
Brad Cantrell <brad777mx@hotmail.com>
2016-12-12 21:28:31 -0800

    autopush

--- game_of_life.rb
+++ game_of_life.rb
@@ -10,11 +10,24 @@ class GoL
   #   board_array
   # end
 
-  def display_board(board_array)
-    result_array=[]
-    board_array.map{|a| result_array << a.join('')}
-    result_array.join("\n")
-  end
+  # def display_board(board_array)
+  #   result_array=[]
+  #   board_array.map{|a| result_array << a.join('')}
+  #   result_array.join("\n")
+  # end
+
+  # def create_occupied_cell_array(brd)
+  #   result_array = []
+  #   brd.each_with_index do |a, x|
+  #     a.each_with_index do |b, y|
+  #       if b == ' X '
+  #         result_array << [x,y]
+  #       end
+  #     end
+  #   end
+  #   result_array
+  # end
+
 
   def create_random_board(n)
     result_array = temp_arr = []
@@ -32,46 +45,33 @@ class GoL
     str = result_array.join("\n")
   end
 
-  def create_occupied_cell_array(brd)
-    result_array = []
-    brd.each_with_index do |a, x|
-      a.each_with_index do |b, y|
-        if b == ' X '
-          result_array << [x,y]
-        end
-      end
-    end
-    result_array
-  end
-
   # create surround array
   # count live cells in that array
   # kill or keep center cell
   # if keep find if birth new cell with other cells
 
+  # def remove_dead_cells(rand_brd, occupied_cell_arr)
+  #   return_board = []
+  #   occupied_cell_arr.each do |a|
+  #     col = a[0]
+  #     row = a[1]
+  #     surr_cells = []
+  #     if col > 0 && row > 0 then surr_cells << rand_brd[col-1][row-1] end
+  #     if col > 0 then surr_cells << rand_brd[col-1][row] end
+  #     if col > 0 && row < 19 then surr_cells << rand_brd[col-1][row+1] end
+  #     if row > 0 then surr_cells << rand_brd[col][row-1] end
+  #     if row < 19 then surr_cells << rand_brd[col][row+1] end
+  #     if col < 19 && row > 0 then surr_cells << rand_brd[col+1][row-1] end
+  #     if col < 19 then surr_cells << rand_brd[col+1][row] end
+  #     if col < 19 && row < 19 then surr_cells << rand_brd[col+1][row+1] end
 
-  def remove_dead_cells(rand_brd, occupied_cell_arr)
-    return_board = []
-    occupied_cell_arr.each do |a|
-      col = a[0]
-      row = a[1]
-      surr_cells = []
-      if col > 0 && row > 0 then surr_cells << rand_brd[col-1][row-1] end
-      if col > 0 then surr_cells << rand_brd[col-1][row] end
-      if col > 0 && row < 19 then surr_cells << rand_brd[col-1][row+1] end
-      if row > 0 then surr_cells << rand_brd[col][row-1] end
-      if row < 19 then surr_cells << rand_brd[col][row+1] end
-      if col < 19 && row > 0 then surr_cells << rand_brd[col+1][row-1] end
-      if col < 19 then surr_cells << rand_brd[col+1][row] end
-      if col < 19 && row < 19 then surr_cells << rand_brd[col+1][row+1] end
-
-      cc = surr_cells.count(' . ') # cc is cell count
-      if cc > 3 || cc < 2
-        rand_brd[col][row] += ' . ' # finish?
-      end
-    end
-    return_board
-  end
+  #     cc = surr_cells.count(' . ') # cc is cell count
+  #     if cc > 3 || cc < 2
+  #       rand_brd[col][row] += ' . ' # finish?
+  #     end
+  #   end
+  #   return_board
+  # end
 
   def reproduce_new_cells(rand_brd, occupied_cells_arr)
   end
